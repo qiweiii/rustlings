95bb11d62858130f8a471062c66ff28b77d098d2
Qiwei Yang <yangqiwei97@gmail.com>
2023-11-11 20:41:02 +0800

    complete

--- exercises/primitive_types/primitive_types3.rs
+++ exercises/primitive_types/primitive_types3.rs
@@ -5,10 +5,19 @@
 // Execute `rustlings hint primitive_types3` or use the `hint` watch subcommand
 // for a hint.
 
-// I AM NOT DONE
-
 fn main() {
-    let a = ???
+    // let a = vec![1; 100];
+    // let a = [1; 100];
+
+    // let mut a: Vec<i32> = Vec::new();
+    // a.resize(100, 0);
+    // a.fill(1);
+
+    let mut a: Vec<i32> = Vec::with_capacity(100);
+    for _ in 0..100 {
+        a.push(1);
+    }
+    println!("{:?}", a.len());
 
     if a.len() >= 100 {
         println!("Wow, that's a big array!");
